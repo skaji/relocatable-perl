95e28041ffdf8d6eb3002ecf04cc6c25096471ac
Shoichi Kaji <skaji@cpan.org>
2019-07-07 02:52:58 +0900

    not fatpack

--- .travis/script_osx.sh
+++ .travis/script_osx.sh
@@ -3,9 +3,12 @@
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
 set -ex
+curl -fsSL https://git.io/perl-install | bash -s ~/perl
+curl -fsSL --compressed -o ~/cpm https://git.io/cpm
+~/perl/bin/perl ~/cpm install -g --cpanfile build/cpanfile
 sudo install -m 755 -o $USER -g staff -d /opt/perl
-perl ./relocatable-perl-build --prefix /opt/perl --perl_version $(cat BUILD_VERSION)
-curl --compressed -sSL https://git.io/cpm | /opt/perl/bin/perl - install -g App::cpanminus App::ChangeShebang
+~/perl/bin/perl build/relocatable-perl-build --prefix /opt/perl --perl_version $(cat BUILD_VERSION)
+/opt/perl/bin/perl ~/cpm install -g App::cpanminus App::ChangeShebang
 /opt/perl/bin/change-shebang -f /opt/perl/bin/*
 
 NAME=perl-$(/opt/perl/bin/perl -MConfig -e 'print $Config{archname}')
