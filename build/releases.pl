98f36a8a5e4dfd4a2a9ff341ce115407dcd25d13
Shoichi Kaji <skaji@cpan.org>
2022-07-31 16:59:04 +0900

    [ci skip] update releases.csv and remove LATEST file

--- build/releases.pl
+++ build/releases.pl
@@ -51,14 +51,20 @@ if ($body->{errors}) {
     die $JSON->encode($body->{errors});
 }
 
+my $new_form = qr{/(?<version>\d+\.\d+\.\d+\.\d+)/perl-(?<os>linux|darwin)-(?<arch>amd64|arm64)\.tar\.(?<compress>gz|xz)$};
+my $old_form = qr{/(?<version>\d+\.\d+\.\d+\.\d+)/perl-(?:(?<arch>x86_64|aarch64)-)?(?<os>linux|darwin).*\.(?<compress>gz|xz)$};
+
 my @release;
 for my $asset (map { $_->{node}{releaseAssets}{edges}->@* } $body->{data}{repository}{releases}{edges}->@*) {
     my $url = $asset->{node}{downloadUrl};
-    if ($url =~ m{/(?<version>\d+\.\d+\.\d+\.\d+)/perl-(?:(?<arch>x86_64|arm64|aarch64)-)?(?<os>linux|darwin).*\.(?<compress>gz|xz)$}) {
+    if ($url =~ $new_form || $url =~ $old_form) {
+        my $arch = $+{arch} || 'amd64';
+        $arch = "amd64" if $arch eq "x86_64";
+        $arch = "arm64" if $arch eq "aarch64";
         push @release, {
             url => $url,
             version => $+{version},
-            arch => ($+{arch} || "x86_64"),
+            arch => $arch,
             os => $+{os},
             compress => $+{compress},
         };
@@ -67,10 +73,13 @@ for my $asset (map { $_->{node}{releaseAssets}{edges}->@* } $body->{data}{reposi
 
 my $sort_by = sub ($a, $b) {
     my %os = (linux => 1, darwin => 0);
+    my %arch = (amd64 => 1, arm64 => 0);
     my %compress = (xz => 1, gz => 0);
     $b->{version} cmp $a->{version}
     ||
     $os{$b->{os}} <=> $os{$a->{os}}
+    ||
+    $arch{$b->{arch}} <=> $arch{$a->{arch}}
     ||
     $compress{$b->{compress}} <=> $compress{$a->{compress}};
 };
